a5f10d0f58253ae16a36aec48acdb86e65fc167d
Nikita <nikitaholimath809@gmail.com>
2026-05-01 02:28:21 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -365,6 +365,7 @@ def download(name, result, confidence, stage):
     return redirect('/' + filename)
 
 # ================= RUN =================
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
