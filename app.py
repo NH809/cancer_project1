be2e8433b1b52db7a7731e0333a0237bee89ea9a
Nikita <nikitaholimath809@gmail.com>
2026-05-01 00:14:17 +0530

    fixed render port

--- app.py
+++ app.py
@@ -329,4 +329,5 @@ def download(name, result, confidence, stage):
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
